8ecdb012d940d6c1a822d2962c8cd1b4b1986112
barestu <fheartnet@gmail.com>
2018-02-26 22:03:54 +0700

    koreksi file exercise 4 switch case

--- exercise-4.js
+++ exercise-4.js
@@ -1,25 +1,30 @@
-var tanggal = parseInt(prompt('Masukkan tanggal! (1 - 31)'));
-var bulan = parseInt(prompt('Masukkan bulan! (1 - 12)'));
-var tahun = parseInt(prompt('Masukkan tahun! (1900 - 2200)'));
+var tanggal = 1; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
+var bulan = 12; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
+var tahun = 2200; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
+var hasil;
 
-if (tanggal > 31) {
-  console.log('Tanggal yang anda masukkan tidak valid!');
-} else if (tahun < 1900 || tahun > 2200) {
-  console.log('Tahun yang anda masukkan tidak valid!');
+if (tanggal < 1 || tanggal > 31) {
+  console.log('Tanggal tidak valid!');
+} else if (bulan < 1 || bulan > 12){
+  console.log('Bulan tidak valid!');
+} else if (tahun < 1900 || tahun > 2200){
+  console.log('Tahun tidak valid!');
 } else {
   switch (bulan) {
-    case 1:   { console.log(tanggal + ' Januari ' + tahun); break; }
-    case 2:   { console.log(tanggal + ' Februari ' + tahun); break; }
-    case 3:   { console.log(tanggal + ' Maret ' + tahun); break; }
-    case 4:   { console.log(tanggal + ' April ' + tahun); break; }
-    case 5:   { console.log(tanggal + ' Mei ' + tahun); break; }
-    case 6:   { console.log(tanggal + ' Juni ' + tahun); break; }
-    case 7:   { console.log(tanggal + ' Juli ' + tahun);break; }
-    case 8:   { console.log(tanggal + ' Agustus ' + tahun); break; }
-    case 9:   { console.log(tanggal + ' September ' + tahun); break; }
-    case 10:  { console.log(tanggal + ' Oktober ' + tahun); break; }
-    case 11:  { console.log(tanggal + ' November ' + tahun); break; }
-    case 12:  { console.log(tanggal + ' Desember ' + tahun); break; }
-    default:  { console.log('Bulan yang anda masukkan tidak valid!'); break; }
+    case 1: { bulan = ' Januari '; break;}
+    case 2: { bulan = ' Februari '; break;}
+    case 3: { bulan = ' Maret '; break;}
+    case 4: { bulan = ' April '; break;}
+    case 5: { bulan = ' Mei '; break;}
+    case 6: { bulan = ' Juni '; break;}
+    case 7: { bulan = ' Juli '; break;}
+    case 8: { bulan = ' Agustus '; break;}
+    case 9: { bulan = ' September '; break;}
+    case 10: { bulan = ' Oktober '; break;}
+    case 11: { bulan = ' November '; break;}
+    default: { bulan = ' Desember '; break;}
   }
+
+  hasil = tanggal + bulan + tahun;
+  console.log(hasil);
 }
